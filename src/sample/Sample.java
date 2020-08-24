60c1a1da5cf99841dd9cea3a50ed6dbd6ebb2f81
shuichi <shu.heihei1209@gmail.com>
2020-08-24 19:34:16 +0900

    test

---
+++ src/sample/Sample.java
@@ -0,0 +1,21 @@
+package sample;
+
+public class Sample {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+      int x = 1;
+
+      switch (x) {
+      case 1:
+        System.out.println("Xの値は1です");
+        break;
+      case 2:
+        System.out.println("Xの値は2です");
+        break;
+      case 3:
+        System.out.println("Xの値は2です");
+        break;
+      }
+   }
+}
