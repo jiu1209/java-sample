b424dc1b775ed137540b468af3c6370368a99ddb
shuichi <shu.heihei1209@gmail.com>
2020-08-25 15:02:17 +0900

    while文練習

--- src/sample/Sample.java
+++ src/sample/Sample.java
@@ -4,18 +4,15 @@ public class Sample {
 
 	public static void main(String[] args) {
 		// TODO 自動生成されたメソッド・スタブ
-      int x = 1;
+		int x = 1;
+		int i = 0;
 
-      switch (x) {
-      case 1:
-        System.out.println("Xの値は1です");
-        break;
-      case 2:
-        System.out.println("Xの値は2です");
-        break;
-      default:
-        System.out.println("Xの値は2です");
-        break;
-      }
+	    do {
+	    	System.out.println(x + "を足します");
+	        i = i + x;
+	        x++;
+
+	    } while (i < 10 );
+	    System.out.println("合計は" + i + "です");
    }
 }
