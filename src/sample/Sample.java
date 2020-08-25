a341f2d06d5183134d3d1499e17bbed907d0beb9
shuichi <shu.heihei1209@gmail.com>
2020-08-25 17:21:38 +0900

    配列とfor

--- src/sample/Sample.java
+++ src/sample/Sample.java
@@ -4,9 +4,20 @@ public class Sample {
 
 	public static void main(String[] args) {
 		// TODO 自動生成されたメソッド・スタブ
-		for (int i = 3; i > 0 ; i--) {
-			System.out.println( i + "秒前");
+	  double[] peopol_Height = new double[5];
+	    peopol_Height[0] = 175.2;
+	    peopol_Height[1] = 172.5;
+	    peopol_Height[2] = 170.5;
+	    peopol_Height[3] = 168.5;
+	    peopol_Height[4] = 170.0;
+
+	  for (int i = 0; i < peopol_Height.length; i++) {
+		System.out.println(peopol_Height[i]);
 	  }
-		System.out.println("発射");
-   }
+
+	  String[] singo = {"赤","黄","青"};
+	    System.out.println(singo[0]);
+	    System.out.println(singo[1]);
+	    System.out.println(singo[2]);
+	}
 }
