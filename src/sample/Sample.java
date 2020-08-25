660938dd1764470f33123917df55d379d261934f
shuichi <shu.heihei1209@gmail.com>
2020-08-25 16:48:23 +0900

    for文

--- src/sample/Sample.java
+++ src/sample/Sample.java
@@ -4,15 +4,9 @@ public class Sample {
 
 	public static void main(String[] args) {
 		// TODO 自動生成されたメソッド・スタブ
-		int x = 1;
-		int i = 0;
-
-	    do {
-	    	System.out.println(x + "を足します");
-	        i = i + x;
-	        x++;
-
-	    } while (i < 10 );
-	    System.out.println("合計は" + i + "です");
+		for (int i = 3; i > 0 ; i--) {
+			System.out.println( i + "秒前");
+	  }
+		System.out.println("発射");
    }
 }
