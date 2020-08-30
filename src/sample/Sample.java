d64eeb7a6080317a5bd1af8dbda4dbbc870784f2
shuichi <shu.heihei1209@gmail.com>
2020-08-30 16:53:24 +0900

    HashSet.練習

--- src/sample/Sample.java
+++ src/sample/Sample.java
@@ -1,44 +1,26 @@
 package sample;
-import java.util.ArrayList;
+import java.util.HashSet;
 
 
 public class Sample {
 
 	public static void main(String[] args) {
 		// TODO 自動生成されたメソッド・スタブ
+	// セットは要素の重複を許さないコレクション。リストのような順番はない
 
-	 //リストの勉強
-	 //ArrayListクラスで遅い場合はLinkedListクラスを使う
-      ArrayList<String> names = new ArrayList<>();
-      // 変数の宣言とnamesにインスタンスの作成して代入
+    HashSet <String> names = new HashSet<>();
+    // namesにインスタンスを代入
 
       names.add("修一");
-      names.add("正敏");
-      // addメソッドで要素のy＼追加ができる
+      names.add("マングース");
+      // リストと同じでaddメソッドで要素の追加。順番がない。
+      // 同じ要素は追加することができない
 
       System.out.println(names);
 
-      names.add(1,"武");
-      // 指定したインデックスに配置する
+      names.remove("修一");
+      // リストと同じくremoveメソッドで要素の削除
 
       System.out.println(names);
-
-      names.remove(0);
-      // removeメソッドで要素の削除
-
-      System.out.println(names);
-
-      String name = names.get(1);
-      // nameにgetメソッドでnamesの中の1の要素を参照
-      // 参照するだけなので、namesは変更されない
-
-      System.out.println(name);
-
-      names.set(1,"古矢");
-      //setメソッドで1の要素を古矢に変更した
-
-      System.out.println(names);
-
-
 	}
 }
